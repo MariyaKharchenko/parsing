d2e60ba9b3049b4b671ed044501551e877609b70
Mariya <mariyaanatolevna86@yandex.ru>
2024-10-12 14:52:21 +0300

    The hw_4.py changed

--- hw_4.py
+++ hw_4.py
@@ -5,34 +5,41 @@ import csv
 headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Mobile Safari/537.36'}
 url = 'https://news.mail.ru/'
 
-response = requests.get(url, headers=headers)
+try:
+    response = requests.get(url, headers=headers)
 
-#Заберем html дерево и вытащим из него нужные нам ссылки
-#(на момент написания кода на сайте их 11, нам нужно столько же)
-dom = html.fromstring(response.text)
-links = dom.xpath("//div[@data-logger='news__MainTopNews']//@href")
-print(len(links))
-
-#Возьмем дерево с каждой ссылки и заберем с него только то, что нам нужно
-#(заголовок новости и краткое описание). Запишем это в словарь. И добавим
-#все такие словари в список.
-news = []
-for link in links:
-    response = requests.get(link, headers=headers)
+    #Заберем html дерево и вытащим из него нужные нам ссылки
+    #(на момент написания кода на сайте их 11, нам нужно столько же)
     dom = html.fromstring(response.text)
-    new = {}
-    news_title = dom.xpath("//header/h1/text()")
-    news_p = dom.xpath("//header//p/text()")[0]
+    links = dom.xpath("//div[@data-logger='news__MainTopNews']//@href")
+    print(len(links))
 
-    new['news_title'] = news_title[0]
-    new['news_p'] = ' '.join(news_p.split())
+    #Возьмем дерево с каждой ссылки и заберем с него только то, что нам нужно
+    #(заголовок новости и краткое описание). Запишем это в словарь. И добавим
+    #все такие словари в список.
+    news = []
+    for link in links:
+        try:
+            response = requests.get(link, headers=headers)
+            dom = html.fromstring(response.text)
+            new = {}
+            news_title = dom.xpath("//header/h1/text()")
+            news_p = dom.xpath("//header//p/text()")[0]
 
-    news.append(new)
+            new['news_title'] = news_title[0]
+            new['news_p'] = ' '.join(news_p.split())
 
-#Запишем данные в csv файл.
-field_names = ['news_title', 'news_p']
-with open("news.csv", "w", encoding='utf8') as w_file:
-    writer = csv.DictWriter(w_file, fieldnames=field_names)
-    writer.writeheader()
-    writer.writerows(news)
+            news.append(new)
+        except requests.exceptions.HTTPError as e:
+            print("Response not 200:", e)
+
+    #Запишем данные в csv файл.
+    field_names = ['news_title', 'news_p']
+    with open("news.csv", "w", encoding='utf8') as w_file:
+        writer = csv.DictWriter(w_file, fieldnames=field_names)
+        writer.writeheader()
+        writer.writerows(news)
+
+except requests.exceptions.HTTPError as e:
+    print("Response not 200:", e)
 
